496a7af7811eebe4c51bbd49446ce7c1039930c2
LuiWorks <u35148@hs-harz.com>
2020-10-23 08:39:49 +0200

    Version 1.0 Prints the wanted amount of prime numbers at the console.

---
+++ SoftwaretechnikTest/src/Main.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Please insert an integer. This number defines the amount of prime numberst to be safed in the file");
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		int number = scanner.nextInt(); 
+		boolean[] primeNumber = PrimeNumber.isPrime(number); 
+		
+//		for (int i = 0; i < primeNumber.length; i++) {
+//			primeNumber[i] = true; 
+//		}
+//		
+//		for (int k = 2; k <= primeNumber.length; k++) {
+//			if (primeNumber[k-1]) {
+//				System.out.println(k);
+//				
+//				for (int j = 2*k; j <= primeNumber.length; j += k) {
+//					primeNumber[j-1] = false; 
+//				}
+//			}
+//		}
+
+		scanner.close();
+	}
+
+}
---
+++ SoftwaretechnikTest/src/PrimeNumber.java
@@ -0,0 +1,25 @@
+
+public class PrimeNumber {
+	
+	public static boolean[] isPrime(int number) {
+		
+		boolean[] primeNumber = new boolean[number];
+		
+		for (int i = 0; i < primeNumber.length; i++) {
+			primeNumber[i] = true; 
+		}
+		
+		for (int k = 2; k <= primeNumber.length; k++) {
+			if (primeNumber[k-1]) {
+				System.out.println(k);
+				
+				for (int j = 2*k; j <= primeNumber.length; j += k) {
+					primeNumber[j-1] = false; 
+				}
+			}
+		}
+		
+		return primeNumber; 
+	}
+	
+}
